9383aa3c6f321d875eb1e3645943545d3bea65d8
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-09-06 20:08:28 +0700

    open l9-learning-nguyenthihuynhnhi

--- app.js
+++ app.js
@@ -8,12 +8,11 @@ const compression = require("compression");
 const helmet = require("helmet");
 const responsesHelper = require("./src/helpers/responsesHelper.js");
 
-
 const app = express();
 
 // Danh sách các đường dẫn được cho phép truy cập
 // const allowedOrigins = ["https://netflix-vulebaolong.netlify.app"];
-const allowedOrigins = ["https://l9-learning-vulebaolong.netlify.app, https://l9-learning-nguyenthihuynhnhi.netlify.app"];
+const allowedOrigins = ["https://l9-learning-vulebaolong.netlify.app", "https://l9-learning-nguyenthihuynhnhi.netlify.app"];
 
 const corsOptions = {
     origin: function (origin, callback) {
@@ -33,20 +32,20 @@ app.use(cors(corsOptions));
 //  ===============MIDLEWARAE =========================
 // bảo vệ cho phép tài nguyên trong ứng dụng của bạn được truy cập từ các nguồn gốc khác nhau
 app.use(
-  helmet({
-      contentSecurityPolicy: true,
-      crossOriginResourcePolicy: { policy: "cross-origin" },
-  })
+    helmet({
+        contentSecurityPolicy: true,
+        crossOriginResourcePolicy: { policy: "cross-origin" },
+    })
 );
 
 // limit giới hạn tần suất các yêu cầu (requests) từ một địa chỉ IP cụ thể
 const limiter = () => {
-  const { result } = responsesHelper(429, "Quá nhiều yêu cầu, vui lòng thử lại sau.");
-  return rateLimit({
-      windowMs: 1000, // 2 giây Thời gian cửa sổ (ms)
-      max: 10, // Số lượng yêu cầu tối đa trong cửa sổ thời gian
-      message: result, // Thông báo lỗi khi vượt quá giới hạn
-  });
+    const { result } = responsesHelper(429, "Quá nhiều yêu cầu, vui lòng thử lại sau.");
+    return rateLimit({
+        windowMs: 1000, // 2 giây Thời gian cửa sổ (ms)
+        max: 10, // Số lượng yêu cầu tối đa trong cửa sổ thời gian
+        message: result, // Thông báo lỗi khi vượt quá giới hạn
+    });
 };
 app.use("/api", limiter());
 
@@ -59,7 +58,6 @@ app.use(compression());
 // express.json(): body => JSON
 app.use(express.json());
 //  ===============MIDLEWARAE =========================
-
 
 app.use("/api/v1", routers);
 
